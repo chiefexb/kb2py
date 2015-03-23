3dcaf46a6bb89c5d6ac4cb841c570ba5f7c91383
chiefexb <chief_exb@mail.ru>
2015-03-23 23:55:36 +0300

    Добавил инструмент для создания MapTiler tileset Для tiled. Создание файлов слотов тайлов из группы изображений

--- kb2rb.py
+++ kb2rb.py
@@ -12,7 +12,7 @@ def main():
 
     pygame.init() # Инициация PyGame, обязательная строчка 
     screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT)) # Создаем окошко
-    pygame.display.set_caption("Super Mario Boy") # Пишем в шапку
+    pygame.display.set_caption("King Bounty 3 Rebirth") # Пишем в шапку
     bg = Surface((WIN_WIDTH,WIN_HEIGHT)) # Создание видимой поверхности
                                          # будем использовать как фон
     bg.fill(Color(BACKGROUND_COLOR))     # Заливаем поверхность сплошным цветом
@@ -21,6 +21,9 @@ def main():
         for e in pygame.event.get(): # Обрабатываем события
             if e.type == QUIT:
                 raise SystemExit, "QUIT"
+        pf=Surface((96,60))
+        pf.image.load('./pic/SUBJECT1_0.png').convert()
+        screen.blit(pf,(0,0))
         screen.blit(bg, (0,0))      # Каждую итерацию необходимо всё перерисовывать 
         pygame.display.update()     # обновление и вывод всех изменений на экран
 if __name__ == "__main__":
